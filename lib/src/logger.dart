bbee662be5e495bb31343b11a06942bedac9fdee
DiskMTH <gillet.mat@free.fr>
2024-02-20 21:41:54 +0100

    add logger

---
+++ lib/src/logger.dart
@@ -0,0 +1,97 @@
+abstract class AbstractLogger {
+  void debug(
+    dynamic message, {
+    List<dynamic> args = const [],
+  });
+
+  void log(
+    dynamic message, {
+    List<dynamic> args = const [],
+  });
+
+  void warn(
+    dynamic message, {
+    Exception? exception,
+    List<dynamic> args = const [],
+  });
+
+  void error(
+    dynamic message, {
+    Exception? exception,
+    List<dynamic> args = const [],
+  });
+}
+
+class Logger implements AbstractLogger {
+  static const String _reset = "\u001b[0m";
+  static const String _cyan = "\u001b[36m";
+  static const String _green = "\u001b[32m";
+  static const String _yellow = "\u001b[33m";
+  static const String _red = "\u001b[31m";
+
+  LogLevel level;
+
+  Logger({this.level = LogLevel.debug});
+
+  @override
+  void debug(
+    dynamic message, {
+    List<dynamic> args = const [],
+  }) {
+    if (level.index <= LogLevel.debug.index) {
+      for (var arg in args) {
+        message = message.replaceFirst("{}", arg.toString());
+      }
+      print("$_cyan${DateTime.now()} - DEBUG: $message$_reset");
+    }
+  }
+
+  @override
+  void log(
+    dynamic message, {
+    List<dynamic> args = const [],
+  }) {
+    if (level.index <= LogLevel.log.index) {
+      for (var arg in args) {
+        message = message.replaceFirst("{}", arg.toString());
+      }
+      print("$_green${DateTime.now()} - LOG: $message$_reset");
+    }
+  }
+
+  @override
+  void warn(
+    dynamic message, {
+    Exception? exception,
+    List<dynamic> args = const [],
+  }) {
+    if (level.index <= LogLevel.warn.index) {
+      for (var arg in args) {
+        message = message.replaceFirst("{}", arg.toString());
+      }
+      print("$_yellow${DateTime.now()} - WARN: $message$_reset");
+      if (exception != null) {
+        print("$_yellow$exception$_reset");
+      }
+    }
+  }
+
+  @override
+  void error(
+    dynamic message, {
+    Exception? exception,
+    List<dynamic> args = const [],
+  }) {
+    if (level.index <= LogLevel.error.index) {
+      for (var arg in args) {
+        message = message.replaceFirst("{}", arg.toString());
+      }
+      print("$_red${DateTime.now()} - ERROR: $message$_reset");
+      if (exception != null) {
+        print("$_red$exception$_reset");
+      }
+    }
+  }
+}
+
+enum LogLevel { debug, log, warn, error }
